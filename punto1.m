2caee12a5996de984250b36546de48ba3077ef0e
ANDRES <edgarerazo.ao@academia.umb.edu.co>
2024-03-17 02:27:08 +0000

    Ejercicios

--- punto1.m
+++ punto1.m
@@ -1,3 +1,1 @@
-%Declarar dos matrices, A y B, de tamaño 4x4 e inicializarlas con valores enteros;
-%aleatorios en el rango [0,10].;
-%operaciones entre martrices;
+
--- punto2.m
+++
@@ -1,3 +0,0 @@
-%Grafica las siguientes funciones en el rango [0,10], usando exactamente 1000 puntos:
-%
-%

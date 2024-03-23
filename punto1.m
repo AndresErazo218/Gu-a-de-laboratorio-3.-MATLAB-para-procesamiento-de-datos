3b20b41fc7679493431edd77a932ef53d38794b5
ANDRES <edgarerazo.ao@academia.umb.edu.co>
2024-03-23 05:27:40 +0000

    arreglo

--- punto1.m
+++ punto1.m
@@ -1,1 +1,8 @@
 
+tamano = input('Digite el tamaño de su matriz n x n ');
+x = input('digite una posición de su matriz en x');
+y = input('digite una posición de su matriz en x');
+A = matriz(tamano,x,y);
+disp('La matriz generada es');
+disp(A);
+

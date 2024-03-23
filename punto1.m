e638cf1ab22bd0fd6956bc9bfe8583bce2aa6bc0
ANDRES <edgarerazo.ao@academia.umb.edu.co>
2024-03-23 05:48:39 +0000

    terminado

--- punto1.m
+++ punto1.m
@@ -1,8 +1,23 @@
-
 tamano = input('Digite el tamaño de su matriz n x n ');
 x = input('digite una posición de su matriz en x');
-y = input('digite una posición de su matriz en x');
+y = input('digite una posición de su matriz en y');
 A = matriz(tamano,x,y);
 disp('La matriz generada es');
 disp(A);
+VMaxFila = max(A, [], 1);
+disp('Vector de salida con el valor máximo de cada fila de A es');
+disp(VMaxFila);
+VPromedio=mean(A, 2);
+disp('Vector de salida con el promedio de cada columna de A');
+disp(VPromedio);
+tamanoA = length(A(:));
+disp('El entero de salida con el número de elementos en A');
+disp(tamanoA);
+Mordenada = sort(A, 2);
+disp('La matriz ordenada es');
+    disp(Mordenada);
+ValorX_Y = Mordenada(x,y);
+disp('El valor de la posicion seleccionada en la matriz ordenada es');
+disp(ValorX_Y);
+
 
